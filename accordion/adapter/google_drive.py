00a5b1060a5129b52f9e9be0f46f1f767771bd40
siyu <siyu@siyusong.com>
2012-09-15 16:59:30 -0400

    fix first bunch of bugs

--- accordion/adapter/google_drive.py
+++ accordion/adapter/google_drive.py
@@ -1,12 +1,17 @@
 import json
+import pprint
+import httplib2
+
 
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
+from oauth2client.client import OAuth2Credentials
+
 
 class GoogleDriveAdapter:
 
   @staticmethod
-  def get_drive_service(auth_info):
+  def _get_drive_service(auth_info):
     """Get an Google Drive service object using stored credential information.
 
     Args:
@@ -17,14 +22,25 @@ class GoogleDriveAdapter:
 
     """
 
-    credentials.from_json(json.load())
+    # j = json.loads()
+    credentials = OAuth2Credentials.from_json(auth_info['credentials'])
     http = httplib2.Http()
     http = credentials.authorize(http)
     
     return build('drive', 'v2', http=http)
 
   @staticmethod
-  def read(self, auth_info, path):
+  def _get_gdfileid_from_id(auth_info, ID):
+    drive_service = GoogleDriveAdapter._get_drive_service(auth_info)
+    param = {'maxResults': 1, 'q': "title = '"+ path + "'", 'fields': 'items/id'}
+    file_list_info = drive_service.files().list(**param).execute()
+    if len(file_list_info['items']) == 0:
+      return None
+    else:
+      return file_list_info['items'][0]['id']
+
+  @staticmethod
+  def read(auth_info, ID):
     """Get the file specified by the path.
 
     Args:
@@ -37,17 +53,21 @@ class GoogleDriveAdapter:
 
     """
 
-    file_id = _get_file_id_from_path(path)
-    file = drive_service.files().get(fileId=file_id).execute()
+    drive_service = GoogleDriveAdapter._get_drive_service(auth_info)
+    file_id = GoogleDriveAdapter._get_gdfileid_from_id(auth_info, path)
     
-    download_url = file['downloadUrl']
-    if download_url:
-      resp, content = drive_service._http.request(download_url)
-      if resp.status == 200:
-        return content
+    if file_id != None:
+      file = drive_service.files().get(fileId=file_id).execute()
+      download_url = file['downloadUrl']
+      if download_url:
+        resp, content = drive_service._http.request(download_url)
+        if resp.status == 200:
+          return content
+    else:
+      return None
     
   @staticmethod
-  def update(self, auth_info, path):
+  def update(auth_info, local_path, ID, overwrite):
     """Upload the file specified by the path. Overwrite if the file already exists.
 
     Args:
@@ -59,6 +79,7 @@ class GoogleDriveAdapter:
 
     """
     
+    drive_service = GoogleDriveAdapter._get_drive_service(auth_info)
     media_body = MediaFileUpload(path, mimetype='binary/octet-stream', resumable=True)
     body = {
       'title': path,
@@ -66,16 +87,16 @@ class GoogleDriveAdapter:
       'mimeType': 'binary/octet-stream'
     }
         
-    file_id = _get_file_id_from_path(path)
+    file_id = _get_gdfileid_from_id(auth_info, path)
     if file_id == None:
-      updated_file = service.files().update(fileId=file_id, body=file, media_body=media_body).execute()
+      updated_file = drive_service.files().update(fileId=file_id, body=file, media_body=media_body).execute()
       return updated_file
     else:    
       inserted_file = drive_service.files().insert(body=body, media_body=media_body).execute()
       return inserted_file
     
   @staticmethod
-  def delete(self, auth_info, path):
+  def delete(auth_info, ID):
     """Delete the file specified by the path
 
     Args:
@@ -87,14 +108,15 @@ class GoogleDriveAdapter:
 
     """
     
+    drive_service = GoogleDriveAdapter._get_drive_service(auth_info)
     param = {'maxResults': 1, 'q': "title = '"+ path + "'", 'fields': 'items/id'}
     file_list_info = drive_service.files().list(**param).execute()
     file_id = file_list_info['items'][0]['id']
-    service.files().delete(fileId=file_id).execute()
+    drive_service.files().delete(fileId=file_id).execute()
     return file_list_info['items'][0]
 
   @staticmethod
-  def metadata(self, auth_info, path):
+  def metadata(auth_info, path):
     """Delete the file specified by the path
 
     Args:
@@ -105,14 +127,8 @@ class GoogleDriveAdapter:
       - ``metadata``: The metadata of the file deleted
 
     """
-    file_id = _get_file_id_from_path(path)
-    file_metadata = service.files().get(fileId=file_id).execute()
+
+    drive_service = GoogleDriveAdapter._get_drive_service(auth_info)
+    file_id = GoogleDriveAdapter._get_gdfileid_from_id(auth_info, path)
+    file_metadata = drive_service.files().get(fileId=file_id).execute()
     return file_metadata
-    
-  def _get_file_id_from_path(path):
-    param = {'maxResults': 1, 'q': "title = '"+ path + "'", 'fields': 'items/id'}
-    file_list_info = drive_service.files().list(**param).execute()
-    if len(file_list_info['items']) == 0:
-      return None
-    else:
-      return file_list_info['items'][0]['id']
