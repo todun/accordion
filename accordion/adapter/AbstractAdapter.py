1374663d606949745a5c92fc9a6fcf56013b143b
Derek Chiang <derekchiang93@gmail.com>
2012-09-15 02:18:40 -0400

    Finished writing DropboxAdapter.py, but hadn't debugged yet.

--- accordion/adapter/AbstractAdapter.py
+++ accordion/adapter/AbstractAdapter.py
@@ -1,16 +1,18 @@
-class AbstractAdapter(object):
+from cherrypy._cprequest import Request
+
+class AbstractAdapter():
     """An abstract adaptor based on a subset of Google Drive and Dropbox APIs"""
-    def get(self, path):
+    def get(self, Request):
         """Retrive a file"""
         raise NotImplementedError("Should have implemented this")
 
-    def update(self, path):
+    def update(self, Request):
         """Update a exisiting file."""
         raise NotImplementedError("Should have implemented this")
     
-    def delete(self, path):
+    def delete(self, Request):
         """Delete a exisiting file."""       
         raise NotImplementedError("Should have implemented this")
         
-    def metadata(self, path):
+    def metadata(self, Request):
     	raise NotImplementedError("Should have implemented this")
