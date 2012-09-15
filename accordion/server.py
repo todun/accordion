d208ce4cc40f3dd5817da864f4712f03768eed5d
Dylan Lukes <lukes.dylan@gmail.com>
2012-09-15 13:48:16 -0400

    ready for heroku (?)

--- accordion/server.py
+++ accordion/server.py
@@ -9,7 +9,6 @@ root = Root()
 conf = {
   'global': {
     'server.socket_host': '0.0.0.0',
-    'server.socket_port': 8000,
   },
   '/': {
     'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
@@ -19,9 +18,11 @@ conf = {
 def main():
   parser = argparse.ArgumentParser(prog='accordion-server', description='Accordion Cloud Aggregation')
 
-  parser.add_argument('-p', '--port', metavar="PORT")
+  parser.add_argument('-p', '--port', metavar="PORT", type=int, required=True)
   parser.add_argument('-c', '--config', metavar="PATH")
 
   args = parser.parse_args()
 
+  cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+  cherrypy.config.update({'server.socket_port': args.port})
   cherrypy.quickstart(root, '/', conf)
