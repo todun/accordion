1f389007175021d8abfb73c24715bc61814610b2
Derek Chiang <derekchiang93@gmail.com>
2012-09-14 23:39:53 -0400

    Add metadata() to AbstractAdapter.py and correct some errors

--- accordion/adapter/AbstractAdapter.py
+++ accordion/adapter/AbstractAdapter.py
@@ -1,14 +1,16 @@
 class AbstractAdapter(object):
     """An abstract adaptor based on a subset of Google Drive and Dropbox APIs"""
-    def get(path):
+    def get(self, path):
         """Retrive a file"""
         raise NotImplementedError("Should have implemented this")
-        
-    def update(path):
+
+    def update(self, path):
         """Update a exisiting file."""
         raise NotImplementedError("Should have implemented this")
     
-    def delete(path):
+    def delete(self, path):
         """Delete a exisiting file."""       
         raise NotImplementedError("Should have implemented this")
         
+    def metadata(self, path):
+    	raise NotImplementedError("Should have implemented this")
