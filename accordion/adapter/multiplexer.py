3b32ca74edfc48ad7b5fd11cfb0ec6e1f669eeb1
Derek Chiang <derekchiang93@gmail.com>
2012-09-15 21:58:35 -0400

    Add multiplexer.py

---
+++ accordion/adapter/multiplexer.py
@@ -0,0 +1,23 @@
+from dropbox import client, rest, session
+
+class Multiplexer():
+
+  @staticmethod
+  def read(auth_info, ID):
+    pass
+
+  @staticmethod
+  def update(auth_info, local_path, ID, overwrite):
+    pass
+
+  @staticmethod
+  def delete(auth_info, ID):
+    pass
+
+  @staticmethod
+  def metadata(auth_info, ID):
+    pass
+
+  @staticmethod
+  def remaining_space(auth_info):
+    pass
