4121ccd007d1b0f67289b4aa2f551bd1bfb7a7d7
Derek Chiang <derekchiang93@gmail.com>
2012-09-16 01:38:43 -0400

    Completed multiplexer.py

--- accordion/adapter/multiplexer.py
+++ accordion/adapter/multiplexer.py
@@ -1,25 +1,29 @@
+import pymongo
+import pprint
+import os
 
+db = pymongo.Connection(os.environ['ACCORDION_MONGO_URI'])
 
 class Multiplexer():
   FileNotFoundExceptionMessage = "The file does not exist."
 
   @staticmethod
   def read(path):
-    f = files.find_one("path": path)
+    f = Multiplexer._get_file(path)
     if f == None:
       raise FileNotFoundException(FileNotFoundExceptionMessage)
     else:
-      adapter = Multiplexer.find_adapter(f.service)
+      adapter = Multiplexer._get_adapter(f.service)
       # Find the file whose name is id
       return adapter.read(Multiplexer._get_auth_info(f), f["_id"])
 
   @staticmethod
-  def update(file_object, path):
-    f = files.find_one("path": path)
+  def update(file_object, path, mime_type="text/plain"):
+    f = Multiplexer._get_file(path)
     if f == None:
-      f = Multiplexer._add_file(file_object, path)
+      f = Multiplexer._add_file(file_object, path, mime_type)
 
-    adapter = Multiplexer._find_adapter(f['service'])
+    adapter = Multiplexer._get_adapter(f['service'])
 
     return adapter.update(Multiplexer._get_auth_info(f), file_object, f['_id'])
 
@@ -29,7 +33,7 @@ class Multiplexer():
     if f == None:
       raise FileNotFoundException(FileNotFoundExceptionMessage)
 
-    adapter = Multiplexer._find_adapter(f['service'])
+    adapter = Multiplexer._get_adapter(f['service'])
     return adapter.delete(Multiplexer._get_auth_info(f), f['_id'])
 
   @staticmethod
@@ -37,38 +41,81 @@ class Multiplexer():
     f = Multiplexer._get_file(path)
     if f == None:
       raise FileNotFoundException(FileNotFoundExceptionMessage)
-    adapter = Multiplexer._find_adapter(f['service'])
+    adapter = Multiplexer._get_adapter(f['service'])
     return adapter.metadata(Multiplexer._get_auth_info(f), f['id'])
 
   @staticmethod
   def remaining_space():
-    
+    total_remaining_space = 0
+    for account in db.accounts.find():
+      adapter = Multiplexer._get_adapter()
+      total_remaining_space += adapter.remaining_space(account['auth_info'])
+    return total_remaining_space
 
   @staticmethod
   def total_space():
-    pass
+    total_total_space = 0
+    for account in db.accounts.find():
+      adapter = Multiplexer._get_adapter()
+      total_total_space += adapter.total_space(account['auth_info'])
+    return total_total_space
 
-  def _add_file(file_object, path):
+  @staticmethod
+  def unused_space():
+    total_unused_space = 0
+    for account in db.accounts.find():
+      adapter = Multiplexer._get_adapter()
+      total_unused_space += adapter.unused_space(account['auth_info'])
+    return total_unused_space
+
+  @staticmethod
+  def _add_file(file_object, path, mime_type):
     """
     Creates a new file object in the collection "files"
     """
-    
+    path = path.split('/')
+    is_dir
+    if file_object == None:
+      is_dir = True
+    else:
+      is_dir = False 
+    for account in db.accounts.find():
+      adapter = Multiplexer._get_adapter(account['service'])
+      if (adapter.unused_space() > Multiplexer._get_size(file_object)):
+        account_id = account['account_id']
+        break
 
-  def _find_adapter(service):
+    f = {"path": path,
+         "is_dir": is_dir,
+         "account_id": account_id,
+         "mime_type": mime_type}
+
+    db.files.insert(f)
+  
+  @staticmethod
+  def _get_size(fileobject):
+    fileobject.seek(0,2) # move the cursor to the end of the file
+    size = fileobject.tell()
+    return size
+
+  @staticmethod
+  def _get_adapter(service):
     """
     Returns an adapter that corresponds to the given service
     """
-    pass
+    
 
+  @staticmethod
   def _get_file(path):
     """
     Returns a dictionary corresponds 
     """
-    return files.find_one("path": path)
+    return db.files.find_one({"path": path})
 
+  @staticmethod
   def _get_auth_info(f):
     """
     Returns auth_info of the account that is in charge of the give file f
     """
-    account = accounts.find_one("_id": f["account_id"])
+    account = db.accounts.find_one({"_id": f["account_id"]})
     return account["auth_info"]
